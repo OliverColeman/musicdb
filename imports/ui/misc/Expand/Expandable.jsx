6499bdf51a8577889f45ace383a6603f63cc812c
Oliver J. Coleman <oliver.coleman@gmail.com>
2018-06-10 14:24:30 +1000

    Minor improvements to Expandable component.

--- imports/ui/misc/Expand/Expandable.jsx
+++ imports/ui/misc/Expand/Expandable.jsx
@@ -13,14 +13,16 @@ class Expandable extends React.Component {
   }
 
   render() {
-    const { children, onToggle, collapsedHeight, showEllipsis, disabled } = this.props;
+    const { children, onToggle, collapsedHeight, collapsedWidth, showEllipsis, disabled } = this.props;
     const { expanded } = this.state;
     return (
       <div className={"Expandable " + (expanded ? 'expanded' : 'collapsed')} style={{height: expanded ? 'auto' : (collapsedHeight || '1.5em')}}>
         { !disabled && <ExpandControl expanded={expanded}
           onToggle={expanded => { this.setState({expanded}); onToggle && onToggle(expanded); }}
         /> }
-        { children }
+        <div className="content" style={{width: !expanded && collapsedWidth ? collapsedWidth : "auto"}}>
+          { children }
+        </div>
         { showEllipsis && <span className="ellipsis">…</span> }
       </div>
     )
